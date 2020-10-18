c7a7e83794afc73a627022777a1955cecf1b24e5
Carlos <carlos4torres_12@hotmail.com>
2020-10-18 17:43:26 -0400

    Added Working Crosshair and Additional Movement
    Added a prototype crosshair follower script that makes a crosshair that follows the player's mouse pointer in game. The part to move the player is still in progress and does not work with the dolly yet.

---
+++ StarGrazer/Assets/Scripts/CrosshairFollower.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairFollower : MonoBehaviour
+{
+    public Transform playerShip;
+    //public float lookDistance = 5;
+
+    //private Plane plane;
+    //private Vector3 distanceFromCam;
+
+    //private void Start()
+    //{
+    //    distanceFromCam = playerShip.position + new Vector3(0, 0, 10);
+    //    // Creates a flat plane in front of the player object
+    //    plane = new Plane(playerShip.forward, distanceFromCam);
+    //}
+
+    private void Update()
+    {
+        FollowMouse();   
+    }
+
+    private void FollowMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        transform.position = mousePos;
+
+        //Sends a ray to track where the cursor is on the plane and adjusts the player model to slightly tilt towards that direction
+        //Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        //float distanceToPoint = 0.0f;
+        //if (plane.Raycast(ray, out distanceToPoint))
+        //{
+        //    Vector3 hitPoint = ray.GetPoint(distanceToPoint);
+        //    hitPoint = Vector3.ClampMagnitude(hitPoint, lookDistance);
+        //    playerShip.LookAt(hitPoint);
+        //}
+    }
+}
