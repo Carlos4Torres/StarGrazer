533721f35ed6f061ff8d22670b849b81217b40c4
Connor Voorhees <cvbor999@gmail.com>
2020-11-01 08:23:10 -0500

    Started Bullet System

---
+++ StarGrazer/Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    //This script contains the function used to summon a bullet prefab with all of its specifications.
+    //First, you would need to have your schedule script reference this one (public gameobject, drag schedule object in inspector, 
+    //get component<'object with this script, let's call it bc')
+    //then, you'd go bc.spawnbullet(arguments);
+
+    public GameObject bulletprefab;
+
+    void spawnbullet ()
+    {
+        GameObject inst = Instantiate(bulletprefab);
+        
+    }
+        
+        
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
