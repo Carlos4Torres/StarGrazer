9e8c29076d2eff0241194b6ec0aab87630f3a16d
cedylanmicheli <cedylanmicheli@gmail.com>
2020-10-31 20:52:54 -0400

    Trying to improve the pathing for enemies a bit

---
+++ StarGrazer/Assets/EnemyTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour
+{
+
+
+    public EnemyMainPath[] mainEnemies;
+    public EnemyDynamicPath[] dynmEnemies;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && mainEnemies != null)
+        {
+            foreach(EnemyMainPath enemy in mainEnemies)
+            {
+                enemy.EnterCombat();
+            }
+        }
+
+        if (other.gameObject.CompareTag("Player") && dynmEnemies != null)
+        {
+            foreach(EnemyDynamicPath enemy in dynmEnemies)
+            {
+                enemy.EnterCombat();
+            }
+        }
+    }
+}
--- StarGrazer/Assets/Scripts/EnemyDynamicPath.cs
+++ StarGrazer/Assets/Scripts/EnemyDynamicPath.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class EnemyMainPath : MonoBehaviour
+public class EnemyDynamicPath : MonoBehaviour
 {
-
     public enum combatState
     {
         IDLE,
@@ -26,6 +25,11 @@ public class EnemyMainPath : MonoBehaviour
     public CinemachineDollyCart mainDollyScipt; //Using this just to match speed
     private CinemachineDollyCart localDollyScript;
 
+    [Header("Scripts and Components")]
+    public CinemachineSmoothPath entryPath;
+    public CinemachineSmoothPath combatPath;
+    public CinemachineSmoothPath escapePath;
+
     void Start()
     {
 
@@ -33,6 +37,7 @@ public class EnemyMainPath : MonoBehaviour
 
         state = combatState.IDLE;
         localDollyScript.m_Speed = 0;
+        localDollyScript.m_Path = entryPath;
     }
 
     void Update()
@@ -42,26 +47,39 @@ public class EnemyMainPath : MonoBehaviour
             case combatState.ENTRY:
 
                 localDollyScript.m_Speed = Mathf.Lerp(localDollyScript.m_Speed, mainDollyScipt.m_Speed, moveDampen);
-                moveDampen += 0.007f * Time.deltaTime;
-                if (mainDollyScipt.m_Speed - localDollyScript.m_Speed < 1)
+                moveDampen += 0.007f * Time.deltaTime; 
+                if (entryPath.PathLength - localDollyScript.m_Position < 1)
                 {
                     state = combatState.COMBAT;
+                    localDollyScript.m_Speed = 0;
+                    localDollyScript.m_Path = combatPath;
+                    localDollyScript.m_Position = 0;
+                    transform.GetChild(0).Rotate(0, 180, 0);
+                    
                     moveDampen = 0;
                 }
                 break;
 
             case combatState.COMBAT:
-                StartCoroutine(Escape());
+                localDollyScript.m_Speed = Mathf.Lerp(localDollyScript.m_Speed, mainDollyScipt.m_Speed, moveDampen);
+                moveDampen += 0.007f * Time.deltaTime;
+                StartCoroutine(TimeToEscape());
                 break;
 
             case combatState.ESCAPE:
+                if(combatPath != escapePath)
+                {
+                    localDollyScript.m_Path = escapePath;
+                }
+                localDollyScript.m_Path = escapePath;
                 localDollyScript.m_Speed = Mathf.Lerp(localDollyScript.m_Speed, escapeSpeed, moveDampen);
-                moveDampen += 0.007f * Time.deltaTime;
+                moveDampen += 0.01f * Time.deltaTime;
                 break;
         }
+        Debug.Log(localDollyScript.m_Speed);
     }
 
-    IEnumerator Escape()
+    IEnumerator TimeToEscape()
     {
         yield return new WaitForSeconds(timeUntilEscape);
         state = combatState.ESCAPE;
