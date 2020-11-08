85af664d724ca53bc308b719f132b9d2746e328a
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2020-11-08 01:15:37 -0500

    Player Shoots at Crosshair (Only for Sidescroller)
    Got the player to shoot at the crosshair in the sidescrolling perspective. Although there are questions to be asked about the design of it.

--- StarGrazer/Assets/Scripts/StarGrazerRotation.cs
+++ StarGrazer/Assets/Scripts/StarGrazerRotation.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Assertions.Must;
 
 public class StarGrazerRotation : MonoBehaviour
@@ -7,9 +8,10 @@ public class StarGrazerRotation : MonoBehaviour
     public float rotateValue;
     public float rotateSpeed;
     public float returnSpeed;
+    public Image crosshair;
 
-    private float rotationX = 0;
-    private float rotationY = 0;
+    //private float rotationX = 0;
+    //private float rotationY = 0;
     private Camera cam;
 
 
@@ -24,35 +26,42 @@ public class StarGrazerRotation : MonoBehaviour
         controls.Gameplay.PlayerMove.performed += ctx => move = ctx.ReadValue<Vector2>();
         controls.Gameplay.PlayerMove.canceled += ctx => move = Vector2.zero;
         cam = Camera.main;
-        point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        //point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.LookAt(point);
+        Vector3 screenPos = cam.WorldToScreenPoint(transform.position);
+        Vector3 screenPosCross = crosshair.rectTransform.position;
+        float angle = Mathf.Atan2(screenPosCross.y - screenPos.y, screenPosCross.x - screenPos.x) * 180 / Mathf.PI;
+        //if (angle < 0)
+        //    angle += 360;
+        Quaternion toAngle = Quaternion.Euler(angle, 180, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toAngle, Time.deltaTime * rotateSpeed);
+        print(angle);
         //Rotater();
     }
 
-    void Rotater()
-    {
-        rotationX += move.x * rotateSpeed * Time.deltaTime; /*Input.GetAxis("Horizontal")*/
-        rotationY +=  move.y * rotateSpeed * Time.deltaTime; /*Input.GetAxis("Vertical")*/
+    //void Rotater()
+    //{
+    //    rotationX += move.x * rotateSpeed * Time.deltaTime; /*Input.GetAxis("Horizontal")*/
+    //    rotationY +=  move.y * rotateSpeed * Time.deltaTime; /*Input.GetAxis("Vertical")*/
 
-        rotationX = Mathf.Clamp(rotationX, -rotateValue, rotateValue);
-        rotationY = Mathf.Clamp(rotationY, -rotateValue, rotateValue);
+    //    rotationX = Mathf.Clamp(rotationX, -rotateValue, rotateValue);
+    //    rotationY = Mathf.Clamp(rotationY, -rotateValue, rotateValue);
         
-        if (Mathf.Approximately(move.x, 0)) /*Input.GetAxis("Horizontal")*/
-        {
-            if (rotationX < 0) rotationX += returnSpeed;
-            if (rotationX > 0) rotationX -= returnSpeed;
-        }
-        if (Mathf.Approximately(move.y, 0)) /*Input.GetAxis("Vertical")*/
-        {
-            if (rotationY < 0) rotationY += returnSpeed;
-            if (rotationY > 0) rotationY -= returnSpeed;
-        }
+    //    if (Mathf.Approximately(move.x, 0)) /*Input.GetAxis("Horizontal")*/
+    //    {
+    //        if (rotationX < 0) rotationX += returnSpeed;
+    //        if (rotationX > 0) rotationX -= returnSpeed;
+    //    }
+    //    if (Mathf.Approximately(move.y, 0)) /*Input.GetAxis("Vertical")*/
+    //    {
+    //        if (rotationY < 0) rotationY += returnSpeed;
+    //        if (rotationY > 0) rotationY -= returnSpeed;
+    //    }
 
-        Vector3 rotationValues = new Vector3(rotationY, transform.localRotation.eulerAngles.y, rotationX);
-        transform.localEulerAngles = rotationValues;
-    }
+    //    Vector3 rotationValues = new Vector3(rotationY, transform.localRotation.eulerAngles.y, rotationX);
+    //    transform.localEulerAngles = rotationValues;
+    //}
 }
