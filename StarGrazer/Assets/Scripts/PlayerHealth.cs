1d18879203c8cfeff8b9b9aea07493594816e53e
Carlos <carlos4torres_12@hotmail.com>
2020-11-09 19:41:14 -0500

    Enemy & Player Update

---
+++ StarGrazer/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    //For archiving purposes, this script must be accompanied by a box collider on CubeGrazer in order to work properly with interaction with enemy bullets.
+
+    public int health;
+
+    public void Demage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
