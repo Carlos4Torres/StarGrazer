8cf2e3932154615616f4b4a137a05e907983f688
cedylanmicheli <cedylanmicheli@gmail.com>
2021-03-15 02:20:05 -0400

    More Visuals Stuff

---
+++ StarGrazer/Assets/Scripts/Environment/BuildingDeleter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingDeleter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject buildings;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(buildings);
+        }
+    }
+}
