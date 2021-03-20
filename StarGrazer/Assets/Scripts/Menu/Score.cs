d1fe76d862532083e215a292941d486765fc5b1b
Carlos <carlos4torres_12@hotmail.com>
2021-03-20 14:13:00 -0400

    Final Score Implementation Update
    This push essentially completes the score counter. I still need to apply another push to carry on the score to the next levels and display a high score.

--- StarGrazer/Assets/Scripts/Menu/Score.cs
+++ StarGrazer/Assets/Scripts/Menu/Score.cs
@@ -6,42 +6,44 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     [SerializeField] private int score;
+    public GameObject[] Enemy;
     [SerializeField] GameObject empty;
-    GameObject[] Enemy;
-
     bool done = false;
 
     public Text scoreText;
+
 
     void Start()
     {
         Enemy = GameObject.FindGameObjectsWithTag("Enemy Model");
     }
 
-
+    //This method uses an array populated with the enemies in each level to run a for loop that constantly checks every frame for a destroyed enemy in the array and adds points to the score variable accordingly
     void Update()
     {
         scoreText.text = score.ToString();
-
-        //This method uses an array populated with the enemies in each level to run a for loop that constantly checks every frame for a destroyed enemy in the array and adds points to the score variable accordingly
-        for(int i=0; i<Enemy.Length; i++)
+        for (int i = 0; i < Enemy.Length; i++)
         {
+            if (Enemy[i] == null)
+            {
+                Enemy[i] = empty;
+            }
+            if (Enemy[i] != Enemy[i] && Enemy[i] != empty)
+            {
+                Enemy[i] = empty;
+            }
+
             if (Enemy[i] != empty)
             {
                 if (Enemy[i].GetComponent<MeshRenderer>().enabled == false)
                 {
 
                     score = score + 10;
-                    Enemy[i] = empty;   
+                    Enemy[i] = empty;
                 }
             }
-          
-        }
 
-        if (!GameObject.FindGameObjectWithTag("Boss") && done == false)
-        {
-            score = score + 100;
-            done = true;
+
         }
 
     }
