67ef1ccfc821404f395fa8514b40a706d8418d3b
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-04-06 00:22:18 -0400

    Fixed one or two issues

--- StarGrazer/Assets/Scripts/Enemies/EnemyHealth.cs
+++ StarGrazer/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -7,11 +7,19 @@ public class EnemyHealth : MonoBehaviour
     [Header("Variables")]
     public EnemyMainPath mainController;
     public AudioSource deathSound;
-    
     public GameObject impactEffect;
 
     public float health = 50;
 
+    private MeshRenderer mesh;
+    private BoxCollider bCollider;
+
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        bCollider = GetComponent<BoxCollider>();
+    }
 
     public void OnTriggerEnter(Collider other)
     {
@@ -31,12 +39,17 @@ public class EnemyHealth : MonoBehaviour
                     Destroy(effectIn, 2f);
 
                     mainController.isDead = true;
-                    var mesh = GetComponent<MeshRenderer>();
                     mesh.enabled = false;
-                    var collider = GetComponent<BoxCollider>();
-                    collider.enabled = false;
+                    bCollider.enabled = false;
                 }
             }
+    }
+
+    public void Reset()
+    {
+        health = 50;
+        mesh.enabled = true;
+        bCollider.enabled = true;
     }
 
   //public void Damage(int damageValue)
